7df58144713b14190592b392a656c05c1c1bfa0d
AugustoV2 <augustovarghese@gmail.com>
2024-12-16 18:28:13 +0530

    push

---
+++ Test/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+import About from './components/TheWelcome.vue'
+
+
+
+const routes = {
+  
+  '/about': About
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value ]
+})
+</script>
+
+<template>
+ 
+  <a href="/about">Aboutaaaa</a> |
+ 
+  <component :is="currentView" />
+</template>
